b4b5a4ba42dad619ebbdca614a2045e778a67af6
robinkashyap <rkashyapp21@gmail.com>
2024-07-28 03:09:01 +0530

    Code Modified

--- 2_Day_Array&2DArray/PushZerosToEnd.py
+++ 2_Day_Array&2DArray/PushZerosToEnd.py
@@ -1,4 +1,21 @@
-
+# Time Complexity - O(n)
+# Space Complexity - O(1)
 
 # push all zeros to end
 
+
+def pushZeros(arr):
+    n = len(arr)
+    count = 0
+    for i in range(n):
+        if arr[i]!=0:
+            arr[count] = arr[i]
+            count = count + 1
+    while(count < n):
+        arr[count] = 0
+        count = count + 1
+    return arr
+
+arr = [3, 4, 0, 16, 0, 0, 17]
+result = pushZeros(arr)     
+print(result)
