ed962f1b66152d48659aa223d45ca12d39044fc8
robinkashyap <rkashyapp21@gmail.com>
2024-07-27 23:31:54 +0530

    Day_3 Code Added

---
+++ 2_Day_Array&2DArray/MergeSortedArray.py
@@ -0,0 +1,37 @@
+# Time Complexity - O(m+n)
+# Space Complexity - O(m+n)
+
+# Merge two sorted array in third array
+
+
+def mergeArray(arr1,arr2):
+    m = len(arr1)
+    n = len(arr2)
+    arr = [0]*(m+n)
+    i = 0
+    j = 0
+    idx = 0
+    while(i < m and j < n):
+        if arr1[i] < arr2[j]:
+            arr[idx] = arr1[i]
+            i = i + 1
+            idx = idx + 1
+        else:
+            arr[idx] = arr2[j]
+            j = j + 1
+            idx = idx + 1
+    while(i < m):
+        arr[idx] = arr1[i]
+        i = i + 1
+        idx = idx + 1
+    while(j < n):
+        arr[idx] = arr2[j]
+        j = j + 1
+        idx = idx + 1
+    return arr
+
+arr1 = [3, 4, 10, 16, 17]
+arr2 = [8, 12, 13]
+
+result = mergeArray(arr1, arr2)
+print(result)
---
+++ 2_Day_Array&2DArray/PushZerosToEnd.py
@@ -0,0 +1,4 @@
+
+
+# push all zeros to end
+
