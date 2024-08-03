8e4a0b1b7507714a2630d6cff227092bfc5c1f63
robinkashyap <rkashyapp21@gmail.com>
2024-08-04 01:23:20 +0530

    modified the code

--- 2_Day_Array&2DArray/Conting1.py
+++ 2_Day_Array&2DArray/Conting1.py
@@ -5,20 +5,36 @@
 
 def counting1(arr):
     n = len(arr)
-    max = 0
+    max_count = 0
     count = 0
+    start_index = -1
+    end_index = -1
+    current_start = 0
+
     for i in range(n):
         if arr[i] == 1:
-            count = count + 1
+            count += 1
+            if count == 1:
+                current_start = i
         else:
+            if count > max_count:
+                max_count = count
+                start_index = current_start
+                end_index = i - 1
             count = 0
-        if max < count:
-            max = count
-    return max
 
-arr = [1, 0, 1, 1, 1, 5, 1, 1, 1, 0, 1, 1]
-result = counting1(arr)
-print(result)
+    # Check one last time in case the array ends with the longest sequence of 1s
+    if count > max_count:
+        max_count = count
+        start_index = current_start
+        end_index = n - 1
+
+    return max_count, start_index, end_index
+
+
+arr = [1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0]
+max, start, end = counting1(arr)
+print(f"max 1's are {max} and starting from {start} to {end}")
         
                    
 
