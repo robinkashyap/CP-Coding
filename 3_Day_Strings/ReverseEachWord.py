8a216ce5fa0d5ed5bcc1a1749a1a66e22156baf2
robinkashyap <rkashyapp21@gmail.com>
2024-08-04 17:45:15 +0530

    code added

---
+++ 3_Day_Strings/ReverseEachWord.py
@@ -0,0 +1,25 @@
+# Time Complexity - O(n)
+# Space Complexity - O(n)
+
+# Reverse each word present in the string
+
+def reverseEachWord(str):
+    arr = list(str)
+    n = len(arr)
+    j = 0
+    def reverseWord(arr,start,end):
+        while(start<=end):
+            arr[start], arr[end] = arr[end], arr[start]
+            start = start + 1
+            end = end - 1
+    for i in range(n):
+        if arr[i] == ' ':
+            reverseWord(arr,j,i-1)
+            j = i + 1
+    reverseWord(arr,j,n-1)
+    return ''.join(arr)
+
+
+str = 'My Name is Robin'
+result = reverseEachWord(str)
+print(result)
