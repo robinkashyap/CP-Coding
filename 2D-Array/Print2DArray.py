559a7c2e3231ddce972a75143f42c0230fa05d1f
robinkashyap <rkashyapp21@gmail.com>
2024-08-07 02:20:55 +0530

    code modified

--- 2D-Array/Print2DArray.py
+++ 2D-Array/Print2DArray.py
@@ -1,12 +1,12 @@
 # How to print 2D array having same col in each list
 
-# list = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
-# row = 3
-# col = 4
-# for i in range(row):
-#     for j in range(col):
-#         print(list[i][j], end=" ")
-#     print()
+list = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+row = 3
+col = 4
+for i in range(row):
+    for j in range(col):
+        print(list[i][j], end=" ")
+    print()
 
 # How to print jagged list
 
@@ -15,3 +15,11 @@ for row in list:
     for ele in row:
         print(ele,end=" ")
     print()
+
+
+# using join operation
+
+list = [[1,2,3,4],[5,6],[7,8,9]]
+for row in list:
+    output = ' '.join([str(ele) for ele in row])
+    print(output)
