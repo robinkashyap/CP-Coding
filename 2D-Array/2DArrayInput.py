ef33d5667348d00ab5d6c3247e486301202d5bea
robinkashyap <rkashyapp21@gmail.com>
2024-08-07 02:00:50 +0530

    code added

---
+++ 2D-Array/2DArrayInput.py
@@ -0,0 +1,7 @@
+# i*col + j - formula two add elements at their position
+
+str = input().split()
+row,col = int(str[0]), int(str[1])
+b = input().split()
+list = [[int(b[i*col + j])for j in range(col)]for i in range(row)]
+print(list)
