405748f3da7bceeba5c57762ed7fc904e56af3b2
robinkashyap <rkashyapp21@gmail.com>
2024-09-24 01:31:01 +0530

    code added

--- Recurssion/Multiplication.py
+++ Recurssion/Multiplication.py
@@ -1,4 +1,10 @@
 def multiplication(n,m):
-    return n * m
+    if n == 0 or m == 0:
+        return 0
+    if n>0:
+        return n + multiplication(n,m-1)
+    else:
+        return multiplication(n,m+1) - n
 
-print(multiplication(3,0))
+
+print(multiplication(3,4))
