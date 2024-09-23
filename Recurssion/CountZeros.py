abbc41008d325539da9721b98c3a67a43d6b45d9
robinkashyap <rkashyapp21@gmail.com>
2024-09-24 02:07:27 +0530

    code added

--- Recurssion/CountZeros.py
+++ Recurssion/CountZeros.py
@@ -1,3 +1,6 @@
+# Time Complexity = O(log(n)) - for both code
+
+# Code 1
 def countZeros(n):
     if n == 0:
         return 1
@@ -5,4 +8,20 @@ def countZeros(n):
         return 0
     return countZeros(n//10) + (n%10 == 0)
 
-print(countZeros(10200))
+# Code 2
+
+def countZero1(n):
+    if n == 0:
+        return 1
+    return helper(n)
+
+def helper(n):
+    if n == 0:
+        return 0
+    if n%10 == 0:
+        return 1 + helper(n//10)
+    else:
+        return helper(n//10)
+
+
+print(countZeros(0))
