89b526e086c1ddbb55f3deb6c92380e4eb625e43
robinkashyap <rkashyapp21@gmail.com>
2024-09-28 02:02:43 +0530

    practice

--- Practice.py
+++ Practice.py
@@ -1,10 +1,12 @@
 def largest(arr):
     n = len(arr)
-    first_largest, second_largest = -1000000, -1000000
+    first_largest = second_largest = float('-inf')
     for i in range(n):
         if arr[i]>first_largest:
             second_largest = first_largest
             first_largest = arr[i]
+        elif arr[i] > second_largest and arr[i] < first_largest:
+            second_largest = arr[i]
     return second_largest
 
 arr = [100, -1, 2, -3, 1, 50]
