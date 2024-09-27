6b82c7d68933f5b3d1881e729331388e75c8c130
robinkashyap <rkashyapp21@gmail.com>
2024-09-28 01:34:41 +0530

    practice

--- Practice.py
+++ Practice.py
@@ -1,21 +1,10 @@
-def counting1(arr):
+def largest(arr):
     n = len(arr)
-    max_count = 0
-    count = 0
+    largest = -1000000
     for i in range(n):
-        if arr[i] == 1:
-            count += 1
-        else:
-            if count > max_count:
-                max_count = count
-            count = 0
-    if count > max_count:
-        max_count = count
-    
-    return count
-                
-        
-arr = [1,1,1,1,0,3,4,1,1,2,1,1,1,1,1,1,1]
-arr1 = [0,0,0,0,0,0,0]
-arr2 = [1,1,1,1,1,1,1]
-print(counting1(arr2))
+        if arr[i]>largest:
+            largest = arr[i]
+    return largest
+
+arr = [0, -1, 2, -3, 1, 50, 100, 100]
+print(largest(arr))
