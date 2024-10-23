9476f9ccfb937ddaa9fb3dfbd69f29884d522e94
robinkashyap <rkashyapp21@gmail.com>
2024-10-23 10:38:08 +0530

    code added

---
+++ DP/MinSteptoReach1_memoization.py
@@ -0,0 +1,32 @@
+import sys
+def minStep(n,dp):
+    if n == 1:
+        return 0
+    ans1,ans2,ans3 = sys.maxsize,sys.maxsize,sys.maxsize
+    if n%3 == 0:
+        if dp[n//3] == -1:
+            ans1 = minStep(n//3,dp)
+            dp[n//3] = ans1
+        else:
+            ans1 = dp[n//3]
+    if n%2 == 0:
+        if dp[n//2] == -1:
+            ans2 = minStep(n//2,dp)
+            dp[n//2] == ans2
+        else:
+            ans2 = dp[n//2]
+    if dp[n-1] == -1:
+        ans3 = minStep(n-1,dp)
+        dp[n-1] = ans3
+    else:
+        ans3 = dp[n-1]
+
+    ans = 1 + min(ans1,ans2,ans3)
+    return ans
+    
+
+
+
+n = int(input())
+dp =[-1 for i in range(n+1)]
+print(minStep(n,dp))
