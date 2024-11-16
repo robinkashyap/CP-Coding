fdbcbee933214016fa3f6f0b8d226e7bf367abdc
robinkashyap <rkashyapp21@gmail.com>
2024-11-16 23:57:41 +0530

    code modification

--- StriverSheet/Intersection.py
+++ StriverSheet/Intersection.py
@@ -1,11 +1,11 @@
 def intersection(arr1,arr2):
     n1 = len(arr1)
     n2 = len(arr2)
-    union = []
+    intersection = []
     i,j = 0,0
     while(i<n1 and j<n2):
         if arr1[i]==arr2[j]:
-            union.append(arr1[i])
+            intersection.append(arr1[i])
             i += 1
             j += 1
         else:
@@ -13,7 +13,7 @@ def intersection(arr1,arr2):
                 i+=1
             else:
                 j+=1
-    return union
+    return intersection
 
 arr1 = [1,2,3,4,4,5,6]
 arr2 = [2,2,2,4,5,6,8,9]
