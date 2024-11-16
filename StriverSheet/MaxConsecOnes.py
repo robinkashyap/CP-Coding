94dcd57eece4e7332bca235d65e5aad8126ca3d5
robinkashyap <rkashyapp21@gmail.com>
2024-11-17 01:59:18 +0530

    code added

---
+++ StriverSheet/MaxConsecOnes.py
@@ -0,0 +1,16 @@
+def maxConsecOnes(arr):
+    n = len(arr)
+    max,count = 0,0
+    for i in range(n):
+        if arr[i]==1:
+            count+=1
+        else:
+            if count>max:
+                max = count
+            count = 0
+    if count>max:
+        max = count
+    return max
+
+arr = [1,1,1,1,1,1,1,1]
+print(maxConsecOnes(arr))
