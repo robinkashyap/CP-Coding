4cba93b91c072fe546db2c8c7cdd223c3cdb5215
robinkashyap <rkashyapp21@gmail.com>
2024-11-18 22:38:18 +0530

    code modification

--- Strings/HighestOccuringCharacter.py
+++ Strings/HighestOccuringCharacter.py
@@ -12,7 +12,7 @@ def highestOccuringChar(str):
         i = i.lower()
         arr[ord(i)-97] += 1
     max = 0
-    k = ''
+    # k = ''
     for j in str:
         if arr[ord(j)-97]>max:
             max = arr[ord(j)-97]
