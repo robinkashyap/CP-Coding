1a3c3763f01d869b8e36127860054cf34033fbd4
robinkashyap <rkashyapp21@gmail.com>
2025-01-23 01:57:03 +0530

    code Practice

--- Practice.py
+++ Practice.py
@@ -1,15 +1,16 @@
-def insertionSort(arr):
-    n = len(arr)
-    for i in range(1,n):
-        temp = arr[i]
-        j = i-1
-        while(j>=0 and arr[j]>temp):
-            arr[j+1] = arr[j]
-            j = j - 1
-        arr[j+1] = temp
-            
-    return arr
+class Vehicle:
+    def __init__(self,color,numGear):
+        self.color = color
+        self.numGear = numGear
 
-arr = [64, 34, 25, 12, 22, 11, 90, 25, 64, 22, 9] # 64,64
-print(insertionSort(arr))
-            
+class Car(Vehicle):
+    def __init__(self,color,numGear,maxSpeed):
+        Vehicle.__init__(self,color,numGear)
+        self.maxSpeed = maxSpeed
+    def printCar(self):
+        print('Color: ',self.color)
+        print('numGear: ',self.numGear)
+        print('maxSpeed: ',self.maxSpeed)
+
+bmw = Car("Black",6,220)
+bmw.printCar()
