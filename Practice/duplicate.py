3448404480e96c931ac3831192be7ca97b934672
robinkashyap <rkashyapp21@gmail.com>
2025-02-08 23:26:54 +0530

    code modified

---
+++ Practice/duplicate.py
@@ -0,0 +1,12 @@
+def removeDup(arr):
+    n = len(arr)
+    i = 0
+    for j in range(i+1,n):
+        if arr[j]!=arr[i]:
+            arr[i+1] = arr[j]
+            i+=1
+    return i
+
+arr = [1, 1, 1, 2, 5, 5, 6, 10, 30]
+res = removeDup(arr)
+print(res)
